9103e598ddadc5ae7b917181171582d522d8294e
Camilla Lobato Pantoja <pantojacamilla@gmail.com>
2021-01-28 20:18:51 +0000

    Adiciona o input element

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const myRequest = new Request('./xml/');
-const myInit = {
-  method: 'GET',
-  headers: { 'Content-Type': 'application/xml' },
-};
+// const myRequest = new Request('./xml/');
+// const myInit = {
+//   method: 'GET',
+//   headers: { 'Content-Type': 'application/xml' },
+// };
 
-fetch(myRequest, myInit)
-  .then((response) => {
-    console.log(response.blob());
-  });
+// fetch(myRequest, myInit)
+//   .then((response) => {
+//     console.log(response.blob());
+//   });
+
+document.getElementById('fileInput').addEventListener('change', selectedFileChanged => {
+  if (this.files.length === 0) {
+    console.log('No file selected.');
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload =  fileReadCompleted => {
+    // when the reader is done, the content is in reader.result.
+    console.log(reader.result);
+  };
+  
+  reader.readAsText(this.files[0]);
+});
