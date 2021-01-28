00290db890852349e8a1e320543eeeb377a1ec7c
Camilla Lobato Pantoja <pantojacamilla@gmail.com>
2021-01-28 20:49:43 +0000

    Adiciona o input element

--- app.js
+++ app.js
@@ -9,17 +9,52 @@
 //     console.log(response.blob());
 //   });
 
-document.getElementById('fileInput').addEventListener('change', selectedFileChanged => {
+document.getElementById('fileInput').addEventListener('change', function selectedFileChanged() {
   if (this.files.length === 0) {
     console.log('No file selected.');
     return;
   }
 
   const reader = new FileReader();
-  reader.onload =  fileReadCompleted => {
-    // when the reader is done, the content is in reader.result.
-    console.log(reader.result);
+  reader.onload = function fileReadCompleted() {
+    //console.log(reader.result);
+
+   
+
   };
+
+ 
+  for( const file of fileInput.files){
+
+      let parser = new DOMParser();
+      let xmlDOM = parser.parseFromString(xmlContent, 'application/xml');
+      let books = xmlDOM.querySelectorAll('vUnCom');
+      let books2 = xmlDOM.querySelectorAll('dhEmi');
+
+      let row = document.createElement('tr');
+
+      //Data
+      let td = document.createElement('td');
+      td.innerText = books.children[0].innerHTML;
+      row.appendChild(td);
+
+      //  Valor
+      td = document.createElement('td');
+      td.innerText = books2.children[1].innerHTML;
+      row.appendChild(td);
+      
+      tableBooks.children[1].appendChild(row);
+  }
+
+   
+
+  //reader.readAsText(this.files[0]);
+  // let parser = new DOMParser();
+  // let xmlDOM = parser.parseFromString(xmlContent, 'application/xml');
+  // let books = xmlDOM.querySelectorAll('book');
+
   
-  reader.readAsText(this.files[0]);
+
+
+
 });
